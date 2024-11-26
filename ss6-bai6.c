4667a29c62bfc383a8af318c8dfa1ca1d88257a8
Hoan06 <167394101+Hoan06@users.noreply.github.com>
2024-11-26 19:20:49 +0700

    btvn_ss6_bai6

--- ss6-bai6.c
+++ ss6-bai6.c
@@ -1,41 +1,20 @@
 #include <stdio.h>
 
-int main (){
+int main(){
+	int i;
 	
-	int day , month;
-	printf ("moi ban nhap ngay sinh ");
-	scanf("%d",&day);
-	printf ("moi ban nhap thang sinh ");
-	scanf("%d",&month);
-	
-	if ((month==1 && day >= 20) || (month==2 && day <= 18)){
-		printf ("ban thuoc cung bao binh");
-	} else if ((month==2 && day>=19)|| (month==3 && day<=20)){
-		printf ("ban thuoc cung song ngu");
-	} else if ((month==3 && day>=21) || (month==4 && day<=20)){
-		printf ("ban thuoc cung bach duong");
-	} else if ((month==4 && day>=21) || (month==5 && day<=20)){
-		printf ("ban thuoc cung kim nguu");
-	} else if ((month==5 && day >=21) || (month==6 && day <=21)){
-		printf ("ban thuoc cung song tu");
-	} else if ((month==6 && day>=22) || (month==7 && day<=22)){
-		printf ("ban thuoc cung cu giai");
-	} else if ((month==7 && day>=23) || (month==8 && day<=22)){
-		printf ("ban thuoc cung su tu");
-	} else if ((month==8 && day>=23) || (month==9 && day<=22)){
-		printf ("ban thuoc cung xu nu");
-	} else if ((month==9 && day>=23) || (month==10 && day<=23)){
-		printf ("ban thuoc cung thien binh");
-	} else if ((month==10 && day>=24) || (month==11 && day<=22)){
-		printf ("ban thuoc cung bo cap");
-	} else if ((month==11 && day>=23) || (month==12 && day<=21)){
-		printf ("ban thuoc cung nhan ma");
-	} else if ((month==12 && day>=22) || (month==1 && day<=19)){
-		printf ("ban thuoc cung ma ket");
-	} else {
-		printf("ngay thang sinh khong hop le");
-	}
-	
+	for (i=0;i<=100;i++){
+		
+		if (i%3==0){
+			printf ("fizz\n");
+		} else if(i%5==0){
+			printf ("buzz\n");
+		} else if (i%33==0 || i%55==0){
+			printf("fizzbuzz\n");
+		} else {
+			printf("%d\n",i);
+		}
+}
 	
 	return 0;
 }
