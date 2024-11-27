f18279796f9ad1c204193a237e53459f2ccffa7c
Hoan06 <167394101+Hoan06@users.noreply.github.com>
2024-11-27 07:45:23 +0700

    btvn_ss6_bai7

--- ss6-bai7.c
+++ ss6-bai7.c
@@ -2,35 +2,20 @@
 
 int main(){
 	
-	int month , year , day;
-	printf("moi ban nhap thang ");
-	scanf("%d",&month);
-	printf("moi ban nhap nam ");
-	scanf("%d",&year);
-	
-	if (month <1 || month >12){
-		printf("khong hop le");
-		return 1;
+	int answer,i;
+	printf("moi ban nhap so nguyen ");
+	scanf("%d",&answer);
+	if (answer<0){
+		answer=-answer;
 	}
-	switch (month){
-		 case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-		 	day=31;
-		 	break;
-		 case 4: case 6: case 9: case 11:
-		 	day=30;
-		 	break;
-		 case 2:
-		 if ((year%4==0 || year%400==0) && year%100!=0){
-		 	day=29;
-		 } else {
-		 	day=28;
-		 }
-		 break;
-	 default :
-	 	day=0;
+	printf ("cac uoc cua %d la ",answer);
+	for (i=0;i<=answer;i++){
+		if(answer%i==0){
+			
+			printf("%d",i);
+		}
 	}
 	
-	printf ("thang %d nam %d co %d ngay", month , year , day);
 	
 	return 0;
 }
